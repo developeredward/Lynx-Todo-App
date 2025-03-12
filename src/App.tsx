4454e5cabc32fdaaf822308e5922f5c34eaea5c3
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-03-12 22:43:10 +0000

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { useCallback, useEffect, useState } from "@lynx-js/react";
 
 import "./styles/App.css";
-import Header from "./components/Header.jsx";
+import HomePage from "./screens/HomePage.jsx";
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false);
-
-  useEffect(() => {
-    console.info("Hello, ReactLynx");
-  }, []);
-
-  const onTap = useCallback(() => {
-    "background only";
-    setAlterLogo(!alterLogo);
-  }, [alterLogo]);
-
   return (
     <view>
       <view className="Background" />
       <view className="App">
-        <Header />
+        <HomePage />
       </view>
     </view>
   );
