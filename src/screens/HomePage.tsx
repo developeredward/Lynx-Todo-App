95d30b6d26f1ef43040385a98b55a8609b8e6421
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-03-14 21:34:39 +0000

    Home Page

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,6 +1,11 @@
+import Card from "../components/Card.jsx";
+import EventCard from "../components/EventCard.jsx";
 import Header from "../components/Header.jsx";
 import Search from "../components/Search.jsx";
 import "../styles/screens/HomePage.css";
+import dance from "../assets/images/dance.jpg";
+import art from "../assets/images/art.jpg";
+
 const HomePage = () => {
   return (
     <view className="container">
@@ -10,6 +15,30 @@ const HomePage = () => {
         <view className="heading">
           <text className="title">Next Agenda</text>
           <text className="link">see all</text>
+        </view>
+        <Card
+          title="Meeting with John"
+          description="Discuss the new project"
+          time="Today, 10:12am"
+          category="Work"
+        />
+        <view className="heading">
+          <text className="title">Events</text>
+          <text className="link">see all</text>
+        </view>
+        <view className="event-cards">
+          <EventCard
+            title="Art Conference"
+            time="Today, 10:00am"
+            image={art}
+            bgColor="#FCF0FE"
+          />
+          <EventCard
+            title="Dance Party"
+            time="Monday, 10:00am"
+            image={dance}
+            bgColor={"#EBFEFB"}
+          />
         </view>
       </view>
     </view>
